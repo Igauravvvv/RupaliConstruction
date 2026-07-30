39d166652e9abd631c721fc763b233f40edcf4a3
Igauravvvv <igauravvvv@gmail.com>
2026-07-30 15:55:40 +0530

    fix: disable vercel default bodyParser so TRPC POST requests do not hang

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,10 @@
 import { handle } from "@hono/node-server/vercel";
 import app from "../server/boot.js";
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 export default handle(app);
